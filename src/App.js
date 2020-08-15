b72d930ee7d9e2d65128c509013e8a3ebffeec4d
Sone <sthaya@gmail.com>
2020-08-15 16:37:39 -0700

    created error checking functionality, app is complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/newsCards/NewsCards';
+import wordsToNumbers from 'words-to-numbers';
 
 import useStyles from './styles.js'
 
@@ -10,15 +11,30 @@ const alanKey = process.env.REACT_APP_ALAN_KEY;
 const App = () => {
 
   const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
 
   const classes = useStyles()
 
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({command, articles}) => {
+      onCommand: ({command, articles, number}) => {
         if (command === 'newHeadlines') {
-          setNewsArticles(articles)
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1)
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+          
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText('Please try that again.'); 
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...')
+          }
         }
       }
     })
@@ -29,7 +45,7 @@ const App = () => {
       <div className={classes.logoContainer}>
         <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo" />
       </div>
-      <NewsCards articles={newsArticles} />
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   )
 }
--- src/components/newsCards/NewsCards.js
+++ src/components/newsCards/NewsCards.js
@@ -11,15 +11,15 @@ const infoCards = [
   { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
 ];
 
-const NewsCards = ({ articles }) => {
+const NewsCards = ({ articles, activeArticle }) => {
   const classes = useStyles();
 
   if (!articles.length) {
     return (
       <Grow in>
         <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-          {infoCards.map((infoCard) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
+          {infoCards.map((infoCard, i) => (
+            <Grid key={i} item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
               <div className={classes.card} style={{backgroundColor: infoCard.color}}>
                 <Typography variant="h5">
                   {infoCard.title}
@@ -47,7 +47,7 @@ const NewsCards = ({ articles }) => {
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
         {articles.map((article, i) => (
           <Grid key={i} item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
-            <NewsCard article={article} i={i} />
+            <NewsCard article={article} activeArticle={activeArticle} i={i} />
           </Grid>
         ))}
       </Grid>
