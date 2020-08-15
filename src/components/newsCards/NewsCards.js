aa069fb619aed309698ef9b5b51a601c1f33745d
Sone <sthaya@gmail.com>
2020-08-14 21:36:28 -0700

    more styling

--- src/components/newsCards/NewsCards.js
+++ src/components/newsCards/NewsCards.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core'
+
+import NewsCard from '../NewsCard/NewsCard'
 
 const NewsCards = ({articles}) => {
   return (
-    <div>
-      {articles.map((article, i) => (
-        <NewsCards />
-      ))}
-    </div>
+    <Grow in>
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+          <Grid key={i} item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
+            <NewsCard article={article} i={i} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
   )
 }
 
